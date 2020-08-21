76b4c1c6da17845f1462268cddb75643203263ad
Santiago <santisayago@gmail.com>
2020-08-21 12:32:40 -0300

    pagina gracias

--- detail.php
+++ detail.php
@@ -115,84 +115,19 @@
                     </div>
                 </div>
             </div>
-            <div class="as-search-results as-filter-open as-category-landing as-desktop" id="as-search-results">
-
-                <div id="accessories-tab" class="as-accessories-details">
-                    <div class="as-accessories" id="as-accessories">
-                        <div class="as-accessories-header">
-                            <div class="as-search-results-count">
-                                <span class="as-search-results-value"></span>
-                            </div>
-                        </div>
-                        <div class="as-searchnav-placeholder" style="height: 77px;">
-                            <div class="row as-search-navbar" id="as-search-navbar" style="width: auto;">
-                                <div class="as-accessories-filter-tile column large-6 small-3">
-
-                                    <button class="as-filter-button" aria-expanded="true" aria-controls="as-search-filters" type="button">
-                                        <h2 class=" as-filter-button-text">
-                                            Smartphones
-                                        </h2>
-                                    </button>
-
-
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="as-accessories-results  as-search-desktop">
-                            <div class="width:60%">
-                                <div class="as-producttile-tilehero with-paddlenav " style="float:left;">
-                                    <div class="as-dummy-container as-dummy-img">
-
-                                        <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " style="max-width: 70%;max-height: 70%;"alt="" width="445" height="445">
-                                    </div>
-                                    <div class="images mini-gallery gal5 ">
-                                    
-
-                                        <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
-                                            <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
-                                                <div class="as-tilegallery-element as-image-selected">
-                                                    <div class=""></div>
-                                                    <img src="<?php echo $_POST['img'] ?>" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
-                                                </div>
-                                                
-                                            </div>
-
-                                            
-                                        </div>
-
-                                        
-
-                                    </div>
-
-                                </div>
-                                <div class="as-producttile-info" style="float:left;min-height: 168px;">
-                                    <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
-                                        <div class="as-producttile-title">
-                                            <h3 class="as-producttile-name">
-                                                <p class="as-producttile-tilelink">
-                                                    <span data-ase-truncate="2"><?php echo $_POST['title'] ?></span>
-                                                </p>
-
-                                            </h3>
-                                        </div>
-                                        <h3 >
-                                            <?php echo $_POST['unit'] ?>
-                                        </h3>
-                                        <h3 >
-                                            <?php echo "$" . $_POST['price'] ?>
-                                        </h3>
-                                    </div>
-                                    <a href="<?php echo $preference->init_point; ?>">Pagar con Mercado Pago</a>
-                                   <!--  <form action="<?php echo $preference->init_point; ?>" method="POST">
-                                    <script data-button-label="Pagar la compra"src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>">
-                                    </script>
-                                   </form> -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <h2>Gracias por tu compra</h2>
+                <?php
+                echo "<p>collection_id: ".$_GET["collection_id"]."</p>";
+                echo "<p>collection_status: ".$_GET["collection_status"]."</p>";
+                echo "<p>external_reference: ".$_GET["external_reference"]."</p>";
+                echo "<p>payment_type: ".$_GET["payment_type"]."</p>";
+                echo "<p>merchant_order_id: ".$_GET["merchant_order_id"]."</p>";
+                echo "<p>preference_id: ".$_GET["preference_id"]."</p>";
+                echo "<p>site_id: ".$_GET["site_id"]."</p>";
+                echo "<p>processing_mode: ".$_GET["processing_mode"]."</p>";
+                echo "<p>merchant_account_id: ".$_GET["merchant_account_id"]."</p>";
+                ?>
             </div>
         </div>
         <div role="alert" class="as-loader-text ally" aria-live="assertive"></div>
