d3ad611a3a919808672a4402714dbae5eb0ea503
Santiago <santisayago@gmail.com>
2020-08-21 11:51:23 -0300

    prueba de webhook

--- webhook.php
+++ webhook.php
@@ -1,5 +1,31 @@
 <?php
-mail('santisayago@gmail.com', 'certificado MP', 'hola');
+require_once 'vendor/autoload.php';
 
+
+    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+    switch($_POST["type"]) {
+        case "payment":
+            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            break;
+        case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+    }
+
+
+$logFile = fopen("log-mp.txt", 'a') or die("Error creando archivo");
+fwrite($logFile, $_POST);
+fclose($logFile);
+
+
+ob_clean();
+header("HTTP/1.1 200 OK");
 
 ?>
