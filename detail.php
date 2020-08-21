66025338f15fbd7d0e83df17b850dab0ca81a62f
Santiago <santisayago@gmail.com>
2020-08-21 11:08:09 -0300

    pruebas 2

--- detail.php
+++ detail.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-    var_dump($_POST);
+    //var_dump($_POST);
     require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
 
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); // Either
--- webhook.php
+++ webhook.php
@@ -1,14 +1,5 @@
 <?php
 mail('santisayago@gmail.com', 'certificado MP', 'hola');
-require_once 'vendor/autoload.php';
 
-   MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"); // Either
-
-$payment = MercadoPago\Payment::find_by_id($id_mp);
-$merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
-
-
-ob_clean();
-header("HTTP/1.1 200 OK");
 
 ?>
