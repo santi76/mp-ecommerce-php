d9e52682dbbd29256e25b3e51d11183bb10b2e9e
Santiago <santisayago@gmail.com>
2020-08-21 12:16:42 -0300

    pruebas 3

--- webhook.php
+++ webhook.php
@@ -4,29 +4,33 @@ require_once 'vendor/autoload.php';
 $json = file_get_contents('php://input');
 
 // Converts it into a PHP object
-$data = json_decode($json);
+$data = json_encode($json);
+
+$logFile = fopen("log-mp-1.txt", 'a') or die("Error creando archivo");
+fwrite($logFile, $data);
+fclose($logFile);
 
 
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
     switch($_POST["type"]) {
         case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            $payment = MercadoPago\Payment::find_by_id($_POST["id"]);
             break;
         case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            $plan = MercadoPago\Plan::find_by_id($_POST["id"]);
             break;
         case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            $plan = MercadoPago\Subscription::find_by_id($_POST["id"]);
             break;
         case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            $plan = MercadoPago\Invoice::find_by_id($_POST["id"]);
             break;
     }
 
 
-$logFile = fopen("log-mp.txt", 'a') or die("Error creando archivo");
-fwrite($logFile, $json);
+$logFile = fopen("log-mp-2.txt", 'a') or die("Error creando archivo");
+fwrite($logFile, $payment);
 fclose($logFile);
 
 
