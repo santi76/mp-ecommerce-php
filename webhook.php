d7de8e1143a2a5be15f0d0655e1a5dbaec61f94d
Santiago <santisayago@gmail.com>
2020-08-21 12:00:04 -0300

    capturar json

--- webhook.php
+++ webhook.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'vendor/autoload.php';
 
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json);
+
 
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
@@ -21,7 +26,7 @@ require_once 'vendor/autoload.php';
 
 
 $logFile = fopen("log-mp.txt", 'a') or die("Error creando archivo");
-fwrite($logFile, $_POST);
+fwrite($logFile, $data);
 fclose($logFile);
 
 
