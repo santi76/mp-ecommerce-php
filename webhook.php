5831002fa6e7d0746362f886cf0ab888bfc979e1
Santiago <santisayago@gmail.com>
2020-08-21 12:27:27 -0300

    prueba con varios archivos

--- webhook.php
+++ webhook.php
@@ -1,40 +1,35 @@
 <?php
-require_once 'vendor/autoload.php';
 
 $json = file_get_contents('php://input');
-
 // Converts it into a PHP object
-$data = json_encode($json);
-
-$logFile = fopen("log-mp-1.txt", 'a') or die("Error creando archivo");
-fwrite($logFile, $data);
+$data = json_decode($json);
+$logFile = fopen("log-mp-json.txt", 'a') or die("Error creando archivo");
+fwrite($logFile, print_r($payment, true));
 fclose($logFile);
 
-
-    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-
-    switch($_POST["type"]) {
-        case "payment":
-            $payment = MercadoPago\Payment::find_by_id($_POST["id"]);
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan::find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription::find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice::find_by_id($_POST["id"]);
-            break;
-    }
+require_once 'vendor/autoload.php';
+MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
 
-$logFile = fopen("log-mp-2.txt", 'a') or die("Error creando archivo");
-fwrite($logFile, $payment);
-fclose($logFile);
+if ($_GET["topic"] == "payment") {
+        
+    $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
+    $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
 
+    //guardo en un archivo payment        
+    $logFile = fopen("log-mp-payment.txt", 'a') or die("Error creando archivo");
+    fwrite($logFile, print_r($payment, true));
+    fclose($logFile);
 
+    //guardo en un archivo merchant_order
+    $logFile = fopen("log-mp-merchant.txt", 'a') or die("Error creando archivo");
+    fwrite($logFile, print_r($merchant_order, true));
+    fclose($logFile);
+}
+
+   
 ob_clean();
 header("HTTP/1.1 200 OK");
 
 ?>
+
